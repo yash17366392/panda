43292af131ac77fac30f71e2162535d1bdf3727e
yash17366392 <yashpandiripalli@gmai.com>
2025-07-09 12:39:30 +0530

    first commit

---
+++ panda.py
@@ -0,0 +1,9 @@
+# Function to multiply two numbers
+def multiply_numbers(a, b):
+    return a * b
+
+# Example usage
+num1 = 6
+num2 = 8
+result = multiply_numbers(num1, num2)
+print("The product is:",result)
--- python.py
+++
@@ -1,9 +0,0 @@
-# Function to add two numbers
-def add_numbers(a, b):
-    return a + b
-
-# Example usage
-num1 = 5
-num2 = 7
-result = add_numbers(num1, num2)
-print("The sum is:", result)
